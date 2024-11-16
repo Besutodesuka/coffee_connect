2ceb7ee43de19d0c0fe709fc6edf3ccbb9f75774
Emma Paris <eparis@gmail.com>
2024-11-16 23:05:31 +0700

    Add packages for reading svg image files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/, // Target .svg files
+        issuer: /\.(js|ts)x?$/, // Process files imported in JS/TS
+        use: ['@svgr/webpack'], // Use the SVGR loader
+      });
+      return config;
+    },
+  };
 
 export default nextConfig;
