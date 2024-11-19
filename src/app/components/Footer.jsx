b0374b1a27a573c3facbc507f90ecb2ca69e433c
ThanooThanu <thanoozaa@gmail.com>
2024-11-19 16:23:04 +0700

    Add svg and adjusting home page and footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer 
-      className="bg-[#333] justify-center items-center p-5 text-white text-center">
+      className="bg-[#312218] justify-center items-center p-5 text-white text-center">
       <div className="flex-grow text-center p-5">
         <div className="flex flex-col items-center">
           <img
@@ -18,8 +18,36 @@ function Footer() {
             We connect coffee farmers and entrepreneurs who are passionate about starting coffee bean-related businesses. By offering quality control, free shipping management and cost, a transparent bidding system, direct trading relations, and the distribution of coffee samples. Coffee Connect ensures a fair solution for sourcing and trading high-quality coffee beans between farmers and entrepreneurs.
           </p>
         </div>
+        <div className="mt-5 flex justify-center items-center space-x-3">
+          <a href="/login">
+            <button className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-gray-100 transition duration-200">
+              Sign up
+            </button>
+          </a>
+          <a href="">
+          <button className="px-6 py-2 bg-white text-black border border-black rounded-full hover:bg-gray-100 transition duration-200">
+            Contact
+          </button>
+          </a>
+        </div>
       </div>
-      <p className="mt-2">Copyright 2024.</p>
+      <div className="mt-5 flex justify-center space-x-12">
+        <img
+          className="w-10 h-10 rounded-full object-cover cursor-pointer"
+          src="/Facebook.svg"
+          alt="Facebook"
+        />
+        <img
+          className="w-10 h-10 rounded-full object-cover cursor-pointer"
+          src="/Instragram.svg"
+          alt="Instragram"
+        />
+        <img
+          className="w-10 h-10 rounded-full object-cover cursor-pointer"
+          src="/Tiktok.svg"
+          alt="Tiktok"
+        />
+      </div>
     </footer>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,11 +15,14 @@ export default function Home() {
         <Navbar session={session} />
         
         {/* First section */}
-        <div className="flex-grow text-center p-5">
+        <div className="flex-grow text-center p-5 bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('/HomeBG.svg')" }}
+        >
           <div className="flex flex-col items-center my-5">
-            <p className="text-4xl"><strong>"We connect happiness"</strong></p>
+            <p className="text-5xl"><strong>"We connect happiness"</strong></p>
+            <br></br>
             <img
-              className="my-5 w-64 h-64 object-cover rounded-full"
+              className="my-5 w-57 h-57 object-cover rounded-full"
               src="/Coffee_Connect.svg"
               alt="Profile"
             />
@@ -28,8 +31,9 @@ export default function Home() {
 
         {/* Second section */}
         <div className="flex-grow text-center p-5">
+          <br></br>
           <h3 className="text-3xl"><strong>What is Coffee Connect?</strong></h3>
-          <p className="text-2xl mt-3">
+          <p className="text-1xl mt-3">
             We connect coffee farmers and entrepreneurs who are passionate about starting coffee bean-related businesses. By offering quality control, free shipping management and cost, a transparent bidding system, direct trading relations, and the distribution of coffee samples. Coffee Connect ensures a fair solution for sourcing and trading high-quality coffee beans between farmers and entrepreneurs.
           </p>
         </div>
