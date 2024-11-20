1bee208de2bdec8c209a573b1fae36f7189117c1
Besutodesuka <besutodesuka@gmail.com>
2024-11-21 00:05:54 +0700

    fix registration bug throught auto increment

--- models/user.js
+++ models/user.js
@@ -3,11 +3,6 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const userSchema = new Schema(
     {
-        id: {
-            type: Number, // Change to Number for auto-incrementing
-            required: false,
-            unique: true // Ensure the id is unique
-        },
         username: {
             type: String,
             required: true
@@ -41,7 +36,7 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
-// userSchema.plugin(AutoIncrement, { inc_field: 'id' });
+userSchema.plugin(AutoIncrement, { inc_field: 'userid' });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
