6bf2a18952321fe06e8798e890e48499ad37531c
ThanooThanu <thanoozaa@gmail.com>
2024-11-20 15:00:04 +0700

    add some navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-function navbar() {
+function Navbar() {
   return (
-    <nav className='bg-[#312218] text-white p-5'>
-        <div className='container mx-auto'>
-            <div className='flex justify-between items-center'>
-                <div>
-                    <Link href="/">home</Link>
-                </div>
-                <ul className='flex'>
-                    <li className='mx-3'><Link href="/login">Sign in</Link></li>
-                    <li className='mx-3'><Link href="/register">Sign up</Link></li>
-                </ul>
-            </div>
+    <nav className="bg-[#312218] text-white p-5">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* Logo and Links */}
+          <div className="flex items-center space-x-5">
+            <img
+              className="w-14 h-14 object-cover rounded-full"
+              src="/Coffee_Connect.svg"
+              alt="Profile"
+            />
+            <Link href="/"className="text-base font-bold">Home</Link>
+            <span>|</span>
+            <Link href="/"className="text-base font-bold">Retail Product</Link>
+            <span>|</span>
+            <Link href="/"className="text-base font-bold">Contact Product</Link>
+            <span>|</span>
+            <Link href="/"className="text-base font-bold">Auction</Link>
+          </div>
+
+          {/* Sign In and Sign Up */}
+          <ul className="flex">
+            <li className="mx-3">
+              <Link href="/login">Sign in</Link>
+            </li>
+            <li className="mx-3">
+              <Link href="/register">Sign up</Link>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default navbar
+export default Navbar;
