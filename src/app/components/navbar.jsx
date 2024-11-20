5c7181ef143c22062683ce2e70efaeb298df30d0
Besutodesuka <besutodesuka@gmail.com>
2024-11-20 11:48:18 +0700

    add logout feature along with profile temporary pages

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import {signOut, useSession} from 'next-auth/react'
 
 function navbar() {
-  return (
+    const {data: session } = useSession();
+    console.log(session);
+    return (
     <nav className='bg-[#333] text-white p-5'>
         <div className='container mx-auto'>
             <div className='flex justify-between items-center'>
@@ -10,8 +13,29 @@ function navbar() {
                     <Link href="/">home</Link>
                 </div>
                 <ul className='flex'>
-                    <li className='mx-3'><Link href="/login">Sign in</Link></li>
-                    <li className='mx-3'><Link href="/register">Sign up</Link></li>
+                    {/* {redirect to user profile} */}
+                    {session && (
+                    <li className='mx-3'>
+                        <Link href="/profile">
+                            Profile
+                        </Link>
+                    </li>
+                    )}
+                    {/* {when the user has not logged in} */}
+                    {!session && (
+                    <div>
+                    <li className='mx-3'>
+                        <Link href="/login">
+                            Sign in
+                        </Link>
+                    </li>
+                    <li className='mx-3'>
+                        <Link href="/register">
+                             Sign up
+                        </Link>
+                    </li>
+                    </div>
+                    )}
                 </ul>
             </div>
         </div>
