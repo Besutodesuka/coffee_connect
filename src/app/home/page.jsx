561bcf7812ef3502e01256489c3b83e68ae4dd4e
Besutodesuka <besutodesuka@gmail.com>
2024-11-21 17:21:47 +0700

    make and preview product card component

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,30 +1,27 @@
 "use client"
 
+//this site will be market place
+
 import Image from "next/image";
 import Container from "../components/Container";
+import CoffeeCard from "../components/ProductCard";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-
-    const { data: session } = useSession();
-    if (!session) redirect("/login");
-    console.log(session)
+  // if user does not logged in they cant be in this page
+  const { data: session } = useSession();
+  if (!session) redirect("/login");
 
   return (
     <main>
       <Container>
         <Navbar session={session} />
-          <div className="flex-grow text-center p-10">
-            <h3 className="text-5xl">Welcome, {session?.user?.name}</h3>
-            <p className="text-2xl mt-3">Your email address: {session?.user?.email}</p>
-            <p className="text-2xl mt-3">Your user role: {session?.user?.role}</p>
-          </div>
+        <CoffeeCard name="test" image={null} grade={3} price={10}/>
         <Footer />
       </Container>
     </main>
-    
   );
 }
