43032ff118cace669a804570e88469d97ef38a36
Ksirawat <sirawat.kuln@mail.kmutt.ac.th>
2024-11-25 17:40:21 +0700

    remove comment

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -7,51 +7,6 @@ import { getSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 
-// const ProfileSection = () => (
-//   <section className="mt-10 mx-auto max-w-4xl p-6 bg-white rounded-lg shadow-md">
-
-//     <h2 className="text-2xl font-bold mb-6 text-[#191c1e]">Profile Information</h2>
-//     <form className="space-y-4">
-//       <div>
-//         <label className="block text-gray-700 text-sm">Full Name</label>
-//         <input
-//           type="text"
-//           className="w-full px-4 py-2 border border-gray-300 rounded-lg"
-//           placeholder="John Doe"
-//         />
-//       </div>
-//       <div>
-//         <label className="block text-gray-700 text-sm">Email</label>
-//         <input
-//           type="email"
-//           className="w-full px-4 py-2 border border-gray-300 rounded-lg"
-//           placeholder="example@gmail.com"
-//         />
-//       </div>
-//       <div>
-//         <label className="block text-gray-700 text-sm">Password</label>
-//         <input
-//           type="password"
-//           className="w-full px-4 py-2 border border-gray-300 rounded-lg"
-//           placeholder="••••••••"
-//         />
-//       </div>
-//       <button type="submit" className="w-full px-4 py-2 bg-[#312218] text-white rounded-lg">
-//         Save Changes
-//       </button>
-//       <Link
-//         href='/api/auth/signout'
-//         onClick={(e) => {
-//             e.preventDefault();
-//             signOut({ callbackUrl: '/' });
-//         }}
-//       >
-//             Sign Out
-//       </Link>
-//     </form>
-//   </section>
-// );
-
 const ProfileSection = () => {
   const [showPaymentModal, setShowPaymentModal] = useState(false);
   const [showAddressModal, setShowAddressModal] = useState(false);
