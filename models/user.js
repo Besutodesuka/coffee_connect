02bc9a830f36df505333ccca3907e658195ab4e8
Ksirawat <sirawat.kuln@mail.kmutt.ac.th>
2024-11-26 23:38:35 +0700

    add location field

--- models/user.js
+++ models/user.js
@@ -1,42 +1,46 @@
 import mongoose, { Schema } from "mongoose";
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        firstname: {
-            type: String,
-            required: true
-        },
-        lastname:{
-            type: String,
-            required: true
-        },
-        dateofbirth: {
-            type: Date,
-            required: true
-        },
-        role: {
-            type: String,
-            required: false,
-            default: "user"
-        },
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    dateofbirth: {
+      type: Date,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: false,
+      default: "user",
+    },
+    location: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
 );
 
-userSchema.plugin(AutoIncrement, { inc_field: 'userid' });
+userSchema.plugin(AutoIncrement, { inc_field: "userid" });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
